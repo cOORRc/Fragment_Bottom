f4423e1deacf09f423047da1ae97c2391b77e6d9
Chayapa <chayapa.pr.58@ubu.ac.th>
2018-10-04 22:19:34 +0700

    Initial commit

---
+++ app/src/main/java/com/example/chayapa/test_map/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.chayapa.test_map;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener
+            mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+
+
+                case R.id.navigation_profile:
+                    loadFragment(new ProfileFragment());
+                    return true;
+                case R.id.navigation_gpx:
+                    loadFragment(new GPXFragment());
+                    return true;
+                case R.id.navigation_movie:
+                    loadFragment(new MovieFragment());
+                    return true;
+
+            }
+
+                return false;
+            }
+        };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //loading the default fragment
+        loadFragment(new MovieFragment());
+
+        //getting bottom navigation view and attaching the listener
+        BottomNavigationView navigation = (BottomNavigationView)findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+    }
+
+    private boolean loadFragment(ProfileFragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit(); //จะบอกว่าสำเร็จกี่ครั้ง
+            return true;
+        }
+        return false;
+    }
+
+    private boolean loadFragment(GPXFragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit(); //จะบอกว่าสำเร็จกี่ครั้ง
+            return true;
+        }
+        return false;
+    }
+
+
+    private boolean loadFragment(MovieFragment fragment) {
+        //switching fragment
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit(); //จะบอกว่าสำเร็จกี่ครั้ง
+            return true;
+        }
+        return false;
+    }
+
+}
